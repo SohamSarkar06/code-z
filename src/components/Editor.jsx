e4667e03eec047aefdfee7491c8ebd0a9e93dcdb
Soham Sarkar <sohamsage0610@gmail.com>
2026-01-01 15:37:15 +0530

    Update Editor.jsx

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -51,7 +51,7 @@ whateverMan:
           setError("");
 
           try {
-            const res = await fetch("http://127.0.0.1:8000/run", {
+            const res = await fetch("code-z-backend-production.up.railway.app", {
               method: "POST",
               headers: { "Content-Type": "application/json" },
               body: JSON.stringify({ code }),
